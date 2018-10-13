ef966e2b52ce26285a3a3f2fd3f6b87b9d33c6ae
Sinem Hasırcıoğlu <02150001068@ogr.inonu.edu.tr>
2018-10-14 02:06:35 +0300

    added registration with reCAPTCHA

--- HaberSepeti.UI/Controllers/AccountController.cs
+++ HaberSepeti.UI/Controllers/AccountController.cs
@@ -1,13 +1,24 @@
 ﻿using HaberSepeti.Core.Infrastructure;
 using HaberSepeti.Data.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace HaberSepeti.UI.Controllers
 {
+    public class CaptchaResultt
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+        [JsonProperty("error-codes")]
+        public List<string> ErrorCodes { get; set; }
+    }
+
     public class AccountController : Controller
     {
         private readonly IKullaniciRepository _kullaniciRepository;
@@ -44,6 +55,67 @@ namespace HaberSepeti.UI.Controllers
             }
             ViewBag.Mesaj = "Geçersiz Email/Şifre girdiniz!";
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Kullanici kullanici)
+        {
+            var KullaniciVarMi = _kullaniciRepository.GetMany(x => x.Email == kullanici.Email).SingleOrDefault();
+            if (KullaniciVarMi != null)
+            {
+                ViewBag.Mesaj = "Bu email kullanılmış !";
+                return View();
+            }
+            Kullanici yeniKullanici = new Kullanici
+            {
+                AdSoyad = kullanici.AdSoyad,
+                Email = kullanici.Email,
+                Sifre = kullanici.Sifre,
+                RolId = 3,
+                KayitTarihi = DateTime.Now,
+                AktifMi = true,
+            };
+
+            var recaptcha = Request.Form["g-recaptcha-response"];
+            const string secretkey = "6LfO72kUAAAAANUJJDtClqUZ4n_ei8_bpK5DuHLs";
+            var restUrl = string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretkey, recaptcha);
+            WebRequest request = WebRequest.Create(restUrl);
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            JsonSerializer serializer = new JsonSerializer();
+            CaptchaResultt result = null;
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                string resultObject = reader.ReadToEnd();
+                result = JsonConvert.DeserializeObject<CaptchaResultt>(resultObject);
+            }
+            if (!result.Success)
+            {
+                ModelState.AddModelError("", "captcha mesajınız hatalı");
+                if (result.ErrorCodes != null)
+                {
+                    ModelState.AddModelError("", result.ErrorCodes[0]);
+                }
+                return View();
+            }
+            else
+            {
+                try
+                {
+                    _kullaniciRepository.Insert(yeniKullanici);
+                    _kullaniciRepository.Save();
+                    return RedirectToAction("Index", "Default");
+                }
+                catch (Exception)
+                {
+                    return View();
+                }
+            }
         }
     }
 }
