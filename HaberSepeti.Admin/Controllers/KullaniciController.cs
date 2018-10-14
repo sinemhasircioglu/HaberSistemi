0f8c82b793d498b178ba7228b483bd2503c76a59
Sinem Hasırcıoğlu <02150001068@ogr.inonu.edu.tr>
2018-10-14 16:26:23 +0300

    added user listing

---
+++ HaberSepeti.Admin/Controllers/KullaniciController.cs
@@ -0,0 +1,28 @@
+﻿using HaberSepeti.Core.Infrastructure;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HaberSepeti.Admin.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        private readonly IKullaniciRepository _kullaniciRepository;
+
+        public KullaniciController(IKullaniciRepository kullaniciRepository)
+        {
+            _kullaniciRepository = kullaniciRepository;
+        }
+
+        public ActionResult Index(int sayfa = 1)
+        {
+            int sayfaBoyutu = 5;
+            var kullaniciListesi = _kullaniciRepository.GetAll().OrderByDescending(x => x.Id).ToPagedList(sayfa, sayfaBoyutu);
+            return View(kullaniciListesi);
+        }
+
+    }
+}
